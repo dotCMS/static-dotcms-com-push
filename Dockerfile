62b60a642d19dfe80d4b6491d6cb255ad9e9b9d3
Victor Alfaro <victor.alfaro@dotcms.com>
2021-01-11 17:58:04 -0600

    Introducing new action to push dotcmsh distro files or javadoc dnudle

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM amazon/aws-cli:latest
+
+LABEL com.dotcms.contact "info@dotcms.com"
+LABEL com.dotcms.vendor "dotCMS LLC"
+LABEL com.dotcms.description "dotCMS Content Management System"
+LABEL com.github.actions.icon "hash"
+LABEL com.github.actions.color "gray-dark"
+
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod 500 /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+bucket='static.dotcms.com'
+type="$1"
+version="$2"
+aws_access_key_id="$3"
+aws_secret_access_key="$4"
+
+echo "[default]
+aws_access_key_id=${aws_access_key_id}
+aws_secret_access_key=${aws_secret_access_key}" > /root/.aws/credentials
+
+case "${type}" in
+  distro)
+    targz_distro_file="dotcms_${version}.tar.gz"
+    zip_distro_file="dotcms_${version}.zip"
+    base_key='/versions/'
+    target_base='./dist-output/'
+    
+    key="${base_key}/${targz_distro_file}"
+    object="${target_base}/${targz_distro_file}"
+    /usr/local/bin/aws put-object --bucket ${bucket} --key ${key} --body ${object}
+
+    key="${base_key}/${zip_distro_file}"
+    object="${target_base}/${zip_distro_file}"
+    /usr/local/bin/aws put-object --bucket ${bucket} --key ${key} --body ${object}
+    ;;
+  javadoc)
+    key="docs/${version}/javadocs"
+    object="./buils/docs/javadoc"
+    /usr/local/bin/aws put-object --bucket ${bucket} --key ${key} --body ${object}
+    ;;
+  *)
+    echo "Invalid type, should be 'distro' or 'javadoc'"
+    exit 1
+    ;;
+esac
