a7461d17c7385796145ed81859706f894f9a5ac6
Victor Alfaro <victor.alfaro@dotcms.com>
2021-01-13 16:54:02 -0600

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 bucket='static.dotcms.com'
-type="$1"
-version="$2"
-aws_access_key_id="$3"
-aws_secret_access_key="$4"
-aws_folder=/root/.aws
-aws_region='us-west-2'
-test_run=$5
+type=$1
+test_run=$2
+version="${DOTCMS_VERSION}"
+aws_access_key_id="${AWS_ACCESS_KEY_ID}"
+aws_secret_access_key="${AWS_SECRET_ACCESS_KEY}"
+
 target_base='./dist-output'
 test_prefix='vico'
 distro_base_key='versions'
@@ -16,20 +15,6 @@ if [[ ${test_run} == true ]]; then
   distro_base_key="${test_prefix}/${distro_base_key}"
   javadoc_base_key="${test_prefix}/${javadoc_base_key}"
 fi
-
-export AWS_EC2_METADATA_DISABLED=true
-
-mkdir ${aws_folder} && chmod 755 ${aws_folder}
-echo "[default]
-aws_access_key_id=${aws_access_key_id}
-aws_secret_access_key=${aws_secret_access_key}" > ${aws_folder}/credentials
-echo "[default]
-region=${aws_region}
-output=json" > ${aws_folder}/config
-
-chmod 644 ${aws_folder}/*
-ls -las ${aws_folder}
-cat ${aws_folder}/*
 
 function s3Push {
   local key=$1
@@ -44,12 +29,11 @@ function s3Push {
 ######################################################################"
   ls -las ${object}
   echo "Executing: /usr/local/bin/aws s3 ls ${bucket}/${key}"
-  /usr/local/bin/aws --debug s3 ls ${bucket}/${key}  
-  
-  #echo "Executing: /usr/local/bin/aws s3api put-object --bucket ${bucket} --key ${key} --body ${object}"
-  #/usr/local/bin/aws --debug s3api put-object --bucket ${bucket} --key ${key} --body ${object}
-  #echo "Executing: /usr/local/bin/aws s3 ls ${bucket}/${key}"
-  #/usr/local/bin/aws --debug s3 ls ${bucket}/${key}
+  aws s3 ls ${bucket}/${key}  
+  echo "Executing: /usr/local/bin/aws s3api put-object --bucket ${bucket} --key ${key} --body ${object}"
+  aws s3api put-object --bucket ${bucket} --key ${key} --body ${object}
+  echo "Executing: /usr/local/bin/aws s3 ls ${bucket}/${key}"
+  aws s3 ls ${bucket}/${key}
 }
 
 function pushDistro {
@@ -62,13 +46,10 @@ function pushJavadoc {
   s3Push ${javadoc_base_key} ./buils/docs/javadoc
 }
 
-/usr/local/bin/aws --version
-#route add blackhole 169.254.169.254
-
 case "${type}" in
   distro)
     pushDistro 'tar.gz'
-    #pushDistro 'zip'
+    pushDistro 'zip'
     ;;
   javadoc)
     pushJavadoc 
@@ -83,5 +64,3 @@ case "${type}" in
     exit 1
     ;;
 esac
-
-#route del blackhole 169.254.169.254
